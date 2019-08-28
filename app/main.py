c6d053e09bbc52247fa9e080efebbccd1c8a9793
huftyuk <github@stevehoughton.org.uk>
2019-08-28 21:22:14 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+dataval = "1"
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/readdata")
+def readdata():
+    return {"dataval":dataval}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
